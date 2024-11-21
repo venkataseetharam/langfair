4bd75526904d8bff367fb9a3480fb5e393bd30a9
Viren Bajaj <BajajV@aetna.com>
2024-11-21 12:40:35 -0500

    handle suppressed exceptions succinctly

--- langfair/generator/generator.py
+++ langfair/generator/generator.py
@@ -50,7 +50,7 @@ class ResponseGenerator:
         self.failure_message = FAILURE_MESSAGE
         self.token_cost_date = TOKEN_COST_DATE
         self.llm = langchain_llm
-        self._validate_exceptions(suppressed_exceptions)
+        self.suppressed_exceptions = suppressed_exceptions
         if max_calls_per_min:
             warnings.warn(
                 "max_calls_per_min is deprecated and will not be used. Use LangChain's `InMemoryRateLimiter` instead",
@@ -274,30 +274,12 @@ class ResponseGenerator:
         try:
             result = await chain.ainvoke([prompt])
             return result
-        except Exception as e:
-            if any(isinstance(e, exc) for exc in self.suppressed_exceptions):
-                return FAILURE_MESSAGE
-            else:
-                raise
+        except Exception as err:
+            if self.suppressed_exceptions is not None:
+                if isinstance(err, self.suppressed_exceptions):
+                    return self.failure_message
+            raise err
 
-    def _validate_exceptions(
-        self, suppressed_exceptions: Union[Tuple[BaseException], BaseException]
-    ) -> None:
-        type_error = "suppressed_exceptions must be a subclass of BaseException or a tuple of subclasses of BaseException"
-        if not suppressed_exceptions:
-            self.suppressed_exceptions = ()
-        elif isinstance(suppressed_exceptions, tuple):
-            if not all(issubclass(item, BaseException) for item in suppressed_exceptions):
-                raise TypeError(type_error)
-            self.suppressed_exceptions = suppressed_exceptions
-        else:
-            try: 
-                if issubclass(suppressed_exceptions, BaseException):
-                    self.suppressed_exceptions = (suppressed_exceptions,)
-                else: 
-                    raise TypeError(type_error)
-            except:
-                raise TypeError(type_error)
             
     @staticmethod
     def _num_tokens_from_messages(
